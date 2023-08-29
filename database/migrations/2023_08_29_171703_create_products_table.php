dd922a26f7080e615cfb5fd8961f4334a7ede446
FaizanKamal7 <57044214+FaizanKamal7@users.noreply.github.com>
2023-08-30 00:23:16 +0500

    Update 2023_08_29_171703_create_products_table.php

--- database/migrations/2023_08_29_171703_create_products_table.php
+++ database/migrations/2023_08_29_171703_create_products_table.php
@@ -11,14 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
-            $table->uuid("id")->primary();
-            $table->string("title");
-            $table->string("description")->nullable(true);
-            $table->string("sku");
-            $table->string("type");
-            $table->decimal("cost_price");
-            $table->string("status");
+         Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->longText('description')->nullable();
+            $table->string('sku')->unique();
+            $table->string('type');
+            $table->decimal('cost_price', 8, 2)->nullable();
+            $table->string('status');
             $table->timestamps();
         });
     }
